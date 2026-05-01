0604eccc44d581967aeee61d1e5aa9bf6bb010fb
ArinLamba <arinlamba5@gmail.com>
2026-05-02 02:47:37 +0530

    completions

--- hooks/queries/use-completions.ts
+++ hooks/queries/use-completions.ts
@@ -7,16 +7,20 @@ import { useQuery } from "@tanstack/react-query";
 // useCompletions.ts
 export const useCompletions = () => {
   const range = useRangeStore(s => s.range);
-  
+  console.log("RANGE:", range);
+
   return useQuery({
     queryKey: ["completions", range],
     queryFn: () => getCompletions(range),
 
-    staleTime: 1000 * 60 * 10, // 10 minutes
+
+    // staleTime: 1000 * 60 * 10, // 10 minutes
+    staleTime: 0,
     gcTime: 1000 * 60 * 30,
 
     refetchOnWindowFocus: false,
-    refetchOnMount: false,
+    // refetchOnMount: false,
+    refetchOnMount: true,
     refetchOnReconnect: false,
   });
 };
